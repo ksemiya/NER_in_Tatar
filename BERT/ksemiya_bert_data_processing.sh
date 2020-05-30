a7d80125892135ad3d3b27ee1ce505062066b285
Kseniya Zakirova <ksemiya@gmail.com>
2020-05-30 16:56:46 +0300

    little fix data processing

--- BERT/ksemiya_bert_data_processing.sh
+++ BERT/ksemiya_bert_data_processing.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #TODO normal if statement
 
-#wget -O all_texts_in_utf8_renamed_analyzed_mv1.02.zip "https://www.dropbox.com/s/akghkvur9akawhn/all_texts_in_utf8_renamed_analyzed_mv1.02.zip?dl=1"
 #curl -L "https://www.dropbox.com/s/kn7foa0ndddlses/ttwiki-bio-only-entity.txt?dl=1" \
 #| tr '\t' ' ' > ttwiki-bio-only-entity.txt 
 python3 bert_data_creating.py
